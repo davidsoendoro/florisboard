8d6fe618bcd8074fbb4258a4a57689bf1aa88720
ryandiwdjj <kryandiw@gmail.com>
2021-09-17 13:54:28 +0700

    add transaction CRUD API call

---
+++ app/src/main/java/com/kokatto/kobold/api/model/basemodel/TransactionModel.kt
@@ -0,0 +1,15 @@
+package com.kokatto.kobold.api.model.basemodel
+
+data class TransactionModel(
+    val _id: String,
+    val address: String,
+    val buyer: String,
+    val channel: String,
+    val deliveryFee: Int,
+    val latestStatus: String,
+    val logistic: String,
+    val notes: String,
+    val payingMethod: String,
+    val phone: String,
+    val price: Int
+)
---
+++ app/src/main/java/com/kokatto/kobold/dashboardcreatetransaction/TransactionViewModel.kt
@@ -0,0 +1,125 @@
+package com.kokatto.kobold.dashboardcreatetransaction
+
+import com.kokatto.kobold.api.Network
+import com.kokatto.kobold.api.model.basemodel.TransactionModel
+import com.kokatto.kobold.api.model.response.BaseResponse
+import com.kokatto.kobold.api.model.response.GetPaginationTransactionResponse
+import com.kokatto.kobold.api.model.response.GetTransactionResponse
+import com.skydoves.sandwich.message
+import com.skydoves.sandwich.onError
+import com.skydoves.sandwich.onException
+import com.skydoves.sandwich.onSuccess
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+
+class TransactionViewModel {
+    val scope = CoroutineScope(Job() + Dispatchers.Main)
+
+
+    fun getChatTemplateList(
+        page: Int = 1,
+        pageSize: Int = 10,
+        search: String = "",
+        onLoading: (Boolean) -> Unit,
+        onSuccess: (GetPaginationTransactionResponse) -> Unit,
+        onError: (String) -> Unit
+    ) {
+        scope.launch {
+//            delay(5000)
+
+            onLoading.invoke(true)
+            val response = Network.transactionApi.getPaginatedTransactionList(page, pageSize, search)
+            response.onSuccess {
+                onLoading.invoke(false)
+                onSuccess.invoke(this.data)
+            }.onError {
+                onLoading.invoke(false)
+                onError.invoke(this.message())
+            }.onException {
+                onLoading.invoke(false)
+                onError.invoke(this.message ?: "Unknown Error")
+            }
+        }
+
+    }
+
+    fun createChatTemplate(
+        createTransactionRequest: TransactionModel,
+        onSuccess: (String) -> Unit,
+        onError: (String) -> Unit
+    ) {
+        scope.launch {
+//            delay(5000)
+            val response = Network.transactionApi.postCreateTransaction(createTransactionRequest)
+            response.onSuccess {
+                onSuccess.invoke(this.data.statusMessage)
+            }.onError {
+                onError.invoke(this.message())
+            }.onException {
+                onError.invoke(this.message ?: "Unknown Error")
+            }
+        }
+    }
+
+    fun findAutotextById(
+        id: String,
+        onSuccess: (GetTransactionResponse) -> Unit,
+        onError: (String) -> Unit
+    ) {
+        scope.launch {
+//            delay(5000)
+            val response = Network.transactionApi.findTransactionById(id)
+            response.onSuccess {
+                onSuccess.invoke(this.data)
+            }.onError {
+                onError.invoke(this.message())
+            }.onException {
+                onError.invoke(this.message ?: "Unknown Error")
+            }
+        }
+    }
+
+    fun updateAutotextById(
+        id: String,
+        requestBody: TransactionModel,
+        onSuccess: (BaseResponse) -> Unit,
+        onError: (String) -> Unit
+    ) {
+        scope.launch {
+//            delay(5000)
+            val response = Network.transactionApi.updateTransactionById(id, requestBody)
+            response.onSuccess {
+                onSuccess.invoke(this.data)
+            }.onError {
+                onError.invoke(this.message())
+            }.onException {
+                onError.invoke(this.message ?: "Unknown Error")
+            }
+        }
+    }
+
+    fun deleteAutotextById(
+        id: String,
+        onSuccess: (BaseResponse) -> Unit,
+        onError: (String) -> Unit
+    ) {
+        scope.launch {
+//            delay(5000)
+            val response = Network.transactionApi.deleteTransactionById(id)
+            response.onSuccess {
+                onSuccess.invoke(this.data)
+            }.onError {
+                onError.invoke(this.message())
+            }.onException {
+                onError.invoke(this.message ?: "Unknown Error")
+            }
+        }
+    }
+
+    fun onDestroy() {
+        scope.cancel()
+    }
+}
