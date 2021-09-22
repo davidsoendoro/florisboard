84e4b8a7fea15b0190aef6bc81f7536bf13059ef
ryandiwdjj <kryandiw@gmail.com>
2021-09-22 16:28:29 +0700

    add http logger (OkHttpClient)

--- app/src/main/java/com/kokatto/kobold/api/RetrofitBuilder.kt
+++ app/src/main/java/com/kokatto/kobold/api/RetrofitBuilder.kt
@@ -1,18 +1,23 @@
 package com.kokatto.kobold.api
 
-import android.app.Application
 import com.google.gson.Gson
 import com.skydoves.sandwich.coroutines.CoroutinesResponseCallAdapterFactory
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 private const val baseUrl: String = "https://xnuhystuwl.execute-api.ap-southeast-1.amazonaws.com/dev/"
+val logging = HttpLoggingInterceptor()
 
 internal inline fun <reified T> createNetwork(
     okHttpClient: OkHttpClient.Builder,
     gson: Gson
 ): T {
+    logging.level = HttpLoggingInterceptor.Level.BODY
+
+    okHttpClient.addInterceptor(logging)
+
     val retrofit = Retrofit.Builder()
         .baseUrl(baseUrl)
         .addConverterFactory(GsonConverterFactory.create(gson))
