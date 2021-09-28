2c2c909cac28039b050505d20d9ee75dd880b4e9
ryandiwdjj <kryandiw@gmail.com>
2021-09-28 11:57:13 +0700

    add check shipping cost activity

---
+++ app/src/main/java/com/kokatto/kobold/dashboardcheckshippingcost/CheckShippingcost.kt
@@ -0,0 +1,13 @@
+package com.kokatto.kobold.dashboardcheckshippingcost
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kokatto.kobold.R
+
+class CheckShippingcost : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_shippingcost)
+
+    }
+}
