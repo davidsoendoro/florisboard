ab52798a3b34f8335b9c48e2893c00c1e54a574e
ryandiwdjj <kryandiw@gmail.com>
2021-09-28 19:35:07 +0700

    add shipping cost UI

---
+++ app/src/main/java/com/kokatto/kobold/checkshippingcost/recycleradapter/ChooseCourierRecyclerAdapter.kt
@@ -0,0 +1,48 @@
+package com.kokatto.kobold.checkshippingcost.recycleradapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.kokatto.kobold.R
+import com.kokatto.kobold.api.model.basemodel.AutoTextModel
+import com.kokatto.kobold.extension.vertical
+
+class ChooseCourierRecyclerAdapter(
+//    val dataList: ArrayList<AutoTextModel>,
+    val onClick: OnClick,
+) : RecyclerView.Adapter<ChooseCourierRecyclerAdapter.ViewHolder>() {
+
+    interface OnClick {
+        fun onClicked(data: AutoTextModel)
+        fun onPackageClicked(int: Int)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return (ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_choose_courier, parent, false)))
+    }
+
+    override fun onBindViewHolder(holder: ChooseCourierRecyclerAdapter.ViewHolder, position: Int) {
+        holder.bindViewHolder(position)
+    }
+
+    override fun getItemCount(): Int {
+        return 5
+    }
+
+    inner class ViewHolder(item: View) : RecyclerView.ViewHolder(item), ChooseCourierPackageRecyclerAdapter.OnClick {
+        private val adapter = ChooseCourierPackageRecyclerAdapter(this)
+        private val choosePackageRecyclerView = item.findViewById<RecyclerView>(R.id.choose_package_recyclerview)
+
+        fun bindViewHolder(data: Int) {
+
+
+            choosePackageRecyclerView.adapter = adapter
+            choosePackageRecyclerView.vertical()
+        }
+
+        override fun onPackageClicked(data: Int) {
+            onClick.onPackageClicked(data)
+        }
+    }
+}
