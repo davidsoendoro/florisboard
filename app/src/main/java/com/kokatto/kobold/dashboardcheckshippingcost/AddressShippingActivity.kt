9f9d17e66429b8a7d802eaecc313f7f8194371af
henki <henki@mail.com>
2021-10-01 02:21:21 +0700

    adding API Courier

--- app/src/main/java/com/kokatto/kobold/dashboardcheckshippingcost/AddressShippingActivity.kt
+++ app/src/main/java/com/kokatto/kobold/dashboardcheckshippingcost/AddressShippingActivity.kt
@@ -2,49 +2,78 @@ package com.kokatto.kobold.dashboardcheckshippingcost
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.kokatto.kobold.R
 import com.kokatto.kobold.api.model.basemodel.DeliveryAddressModel
 import com.kokatto.kobold.checkshippingcost.ShippingCostViewModel
 import com.kokatto.kobold.constant.ActivityConstantCode
 import com.kokatto.kobold.dashboardcheckshippingcost.adapter.ShippingAddressRecyclerAdapter
-import com.kokatto.kobold.databinding.ActivitySearchAddressBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.coroutines.CoroutineContext
 
-class AddressShippingActivity : AppCompatActivity() {
+class AddressShippingActivity : AppCompatActivity(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext = Dispatchers.Main
 
     companion object {
         val SENDER = "SENDER"
         val RECEIVER = "RECEIVER"
     }
 
-    lateinit var uiBinding: ActivitySearchAddressBinding
+    private var buttonBack: ImageButton? = null
+    private var buttonClear: ImageView? = null
+    private var searchResultFound: LinearLayout? = null
+    private var searchResultNotFoundLayout: LinearLayout? = null
+    private var searchEdittext: EditText? = null
+    private var fullscreenLoading: LinearLayout? = null
 
     private var dataList: ArrayList<DeliveryAddressModel> = arrayListOf()
+    private var recyclerView: RecyclerView? = null
     private var recyclerAdapter: ShippingAddressRecyclerAdapter? = null
     private var shippingCostViewModel: ShippingCostViewModel? = ShippingCostViewModel()
     private val isLoading = AtomicBoolean(true)
     private var mode: String? = null
 
+    private var textChangedJob: Job? = null
+    private lateinit var textListener: TextWatcher
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        uiBinding = ActivitySearchAddressBinding.inflate(layoutInflater).apply {
-            setContentView(root)
-        }
+        setContentView(R.layout.activity_search_address)
 
-        uiBinding.backButton.setOnClickListener {
+        buttonBack = findViewById(R.id.back_button)
+        buttonClear = findViewById(R.id.clear_button)
+        searchEdittext = findViewById(R.id.search_edittext)
+        searchResultNotFoundLayout = findViewById(R.id.search_result_not_found_layout)
+        fullscreenLoading = findViewById(R.id.fullscreen_loading)
+        recyclerView = findViewById(R.id.recycler_view)
+
+        buttonBack?.setOnClickListener {
             onBackPressed()
         }
 
-        uiBinding.clearButton.setOnClickListener {
-
+        buttonClear?.setOnClickListener {
+            searchEdittext?.setText("")
         }
 
-        uiBinding.recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView?.layoutManager = LinearLayoutManager(this)
         recyclerAdapter = ShippingAddressRecyclerAdapter(this, dataList)
-        uiBinding.recyclerView.adapter = recyclerAdapter
+        recyclerView?.adapter = recyclerAdapter
 
         recyclerAdapter?.onItemClick = {
             when (mode) {
@@ -63,21 +92,57 @@ class AddressShippingActivity : AppCompatActivity() {
             }
         }
 
-        callAPIShippingAddress()
+        textListener = object : TextWatcher {
+            private var searchFor = searchEdittext?.text.toString()
+
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val searchText = s.toString().trim()
+                if (searchText != searchFor) {
+                    searchFor = searchText
+                    buttonClear?.isVisible = true
+                    textChangedJob?.cancel()
+                    textChangedJob = launch(Dispatchers.Main) {
+                        delay(700L)
+                        if (searchText == searchFor) {
+                            callAPIShippingAddress(searchText)
+                        }
+                    }
+                }
+            }
+        }
+
         initialView()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        searchEdittext?.addTextChangedListener(textListener)
+    }
+
+    override fun onPause() {
+        searchEdittext?.removeTextChangedListener(textListener)
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        textChangedJob?.cancel()
+        super.onDestroy()
     }
 
     fun initialView() {
         mode = intent.getStringExtra(ActivityConstantCode.EXTRA_MODE)
-        uiBinding.fullcreenLoading.isVisible = false
-        uiBinding.recyclerView.isVisible = false
-        uiBinding.searchResultNotFoundLayout.isVisible = true
+        fullscreenLoading?.isVisible = false
+        recyclerView?.isVisible = false
+        searchResultNotFoundLayout?.isVisible = true
+        buttonClear?.isVisible = false
     }
 
     private fun callAPIShippingAddress(address: String = "") {
-        uiBinding.searchResultNotFoundLayout.isVisible = false
-        uiBinding.fullcreenLoading.isVisible = true
-
+        searchResultNotFoundLayout?.isVisible = false
+        fullscreenLoading?.isVisible = true
+        dataList.clear()
         shippingCostViewModel?.getPaginatedDeliveryAddress(
             page = 1,
             pageSize = 15,
@@ -91,18 +156,32 @@ class AddressShippingActivity : AppCompatActivity() {
                     dataList.addAll(it.data.contents)
                     recyclerAdapter!!.notifyDataSetChanged()
 
-                    uiBinding.recyclerView.isVisible = true
-                    uiBinding.searchResultNotFoundLayout.isVisible = false
+                    recyclerView?.isVisible = true
+                    searchResultNotFoundLayout?.isVisible = false
                 }
 
-                uiBinding.fullcreenLoading.isVisible = false
+                fullscreenLoading?.isVisible = false
             },
             onError = {
-                uiBinding.fullcreenLoading.isVisible = false
-                uiBinding.recyclerView.isVisible = false
-                uiBinding.searchResultNotFoundLayout.isVisible = true
+                fullscreenLoading?.isVisible = false
+                recyclerView?.isVisible = false
+                searchResultNotFoundLayout?.isVisible = true
             }
         )
     }
 
+//    private fun <T> debounce(
+//        waitMs: Long = 300L,
+//        scope: CoroutineScope,
+//        destinationFunction: (T) -> Unit
+//    ): (T) -> Unit {
+//        var debounceJob: Job? = null
+//        return { param: T ->
+//            debounceJob?.cancel()
+//            debounceJob = scope.launch {
+//                delay(waitMs)
+//                destinationFunction(param)
+//            }
+//        }
+//    }
 }
