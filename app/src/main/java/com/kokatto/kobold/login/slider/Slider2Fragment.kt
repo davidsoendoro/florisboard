6868dca75de594c84c31f7a02f0eb410ef905a9c
henki <henki@mail.com>
2021-10-02 17:41:29 +0700

    adding into layout

---
+++ app/src/main/java/com/kokatto/kobold/login/slider/Slider2Fragment.kt
@@ -0,0 +1,18 @@
+package com.kokatto.kobold.login.slider
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.kokatto.kobold.R
+
+class Slider2Fragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_intro_slider2, container, false)
+
+}
