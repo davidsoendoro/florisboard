29d7da796d19b60efa4f3aca397fcca65d4f82d3
henki <henki@mail.com>
2021-10-04 09:23:44 +0700

    adding OTP input layout

---
+++ app/src/main/java/com/kokatto/kobold/login/OtpActivity.kt
@@ -0,0 +1,14 @@
+package com.kokatto.kobold.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kokatto.kobold.R
+class OtpActivity: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp)
+
+    }
+}
