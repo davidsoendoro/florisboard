94e284678a2ceaefdf4cd684b6d3bc9e32a728c5
YohanesChan <chandrayh24@gmail.com>
2021-10-13 15:47:20 +0700

    Intent Setting Ke menu lain

--- app/src/main/java/com/kokatto/kobold/setting/SettingActivity.kt
+++ app/src/main/java/com/kokatto/kobold/setting/SettingActivity.kt
@@ -1,23 +1,42 @@
 package com.kokatto.kobold.setting
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.card.MaterialCardView
+import com.google.android.play.core.review.ReviewInfo
+import com.google.android.play.core.review.ReviewManager
+import com.google.android.play.core.review.ReviewManagerFactory
 import com.kokatto.kobold.R
+import com.kokatto.kobold.bank.BankHomeActivity
+import com.kokatto.kobold.dashboard.DashboardActivity
 import com.kokatto.kobold.databinding.SettingsActivityBinding
 import com.kokatto.kobold.extension.createBottomSheetDialog
 
 class SettingActivity : AppCompatActivity() {
     private lateinit var ivProfilToko: ImageView
     private lateinit var btnLogOut: Button
+    private lateinit var rlBankAccount: RelativeLayout
+    private lateinit var rlGiveRating: RelativeLayout
+    private lateinit var rlTermCondition: RelativeLayout
+    private lateinit var llShareApp: LinearLayout
     lateinit var uiBinding: SettingsActivityBinding
+
+    private var reviewInfo: ReviewInfo? = null
+    private lateinit var reviewManager: ReviewManager
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
+
 
         uiBinding = SettingsActivityBinding.inflate(layoutInflater).apply {
             setContentView(root)
@@ -36,6 +55,29 @@ class SettingActivity : AppCompatActivity() {
             createConfirmationDialog();
         }
 
+        rlBankAccount = findViewById(R.id.kubold_open_bank_account)
+        rlBankAccount.setOnClickListener{
+            startActivity(Intent(this@SettingActivity, BankHomeActivity::class.java))
+        }
+
+        llShareApp = findViewById(R.id.kubold_card_share)
+        llShareApp.setOnClickListener{
+            createShareDialog();
+        }
+
+        rlGiveRating = findViewById(R.id.kubold_open_give_rating)
+        rlGiveRating.setOnClickListener{
+            try {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=xyz.appmaker.cqshec")))
+            } catch (e: ActivityNotFoundException) {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=xyz.appmaker.cqshec")))
+            }
+        }
+
+        rlTermCondition = findViewById(R.id.kubold_open_term_conditions)
+        rlTermCondition.setOnClickListener{
+
+        }
 
 
     }
@@ -48,11 +90,13 @@ class SettingActivity : AppCompatActivity() {
             )
         )
 
-        val acceptButton = bottomDialog.findViewById<MaterialCardView>(R.id.confirm_button)
+        val acceptButton = bottomDialog.findViewById<MaterialCardView>(R.id.logout_button)
         val discardButton = bottomDialog.findViewById<MaterialCardView>(R.id.cancel_button)
 
         acceptButton?.setOnClickListener {
-
+            val intent = Intent(this, DashboardActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
 
             bottomDialog.dismiss()
         }
@@ -63,5 +107,40 @@ class SettingActivity : AppCompatActivity() {
 
         bottomDialog.show()
     }
+
+    private fun createShareDialog() {
+        val bottomDialog = createBottomSheetDialog(
+            layoutInflater.inflate(
+                R.layout.dialog_confirm_share,
+                null
+            )
+        )
+
+        val acceptButton = bottomDialog.findViewById<MaterialCardView>(R.id.share_button)
+        val discardButton = bottomDialog.findViewById<MaterialCardView>(R.id.cancel_button)
+
+        acceptButton?.setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+
+            bottomDialog.dismiss()
+        }
+
+        discardButton?.setOnClickListener {
+            bottomDialog.dismiss()
+        }
+
+        bottomDialog.show()
+    }
+
+
+
 }
+
 
