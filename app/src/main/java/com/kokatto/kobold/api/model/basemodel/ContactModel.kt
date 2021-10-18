2d30b34f3a81bfe10fca02ca856b1fd6fd9e6308
ryandiwdjj <kryandiw@gmail.com>
2021-10-18 18:19:37 +0700

    add import contact from device

--- app/src/main/java/com/kokatto/kobold/api/model/basemodel/ContactModel.kt
+++ app/src/main/java/com/kokatto/kobold/api/model/basemodel/ContactModel.kt
@@ -1,16 +1,70 @@
 package com.kokatto.kobold.api.model.basemodel
 
+import android.content.ContentResolver
+import android.content.Context
 import android.os.Parcelable
+import android.provider.ContactsContract
 import kotlinx.parcelize.Parcelize
+
 
 @Parcelize
 data class ContactModel(
-    var _id: String,
+    var _id: String = "",
     var name: String,
     var phoneNumber: String,
-    var email: String,
-    var address: String,
+    var email: String = "",
+    var address: String = "",
     var debt: Double = 0.0,
-    var channels: List<ContactChannelModel>,
-    var shippingAddress: List<ContactAddressModel>,
+    var channels: List<ContactChannelModel> = arrayListOf(),
+    var shippingAddress: List<ContactAddressModel> = arrayListOf(),
 ) : Parcelable
+
+private val PROJECTION = arrayOf(
+    ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
+    ContactsContract.Contacts.DISPLAY_NAME,
+    ContactsContract.CommonDataKinds.Phone.NUMBER
+)
+
+fun getContactList(context: Context): ArrayList<ContactModel> {
+    var contactList: ArrayList<ContactModel> = ArrayList()
+
+    val cr: ContentResolver = context.contentResolver
+    val cursor = cr.query(
+        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+        PROJECTION,
+        null,
+        null,
+        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC"
+    )
+    if (cursor != null) {
+        val mobileNoSet = HashSet<String>()
+        try {
+            val nameIndex = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+            val numberIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+            var name: String
+            var number: String
+            while (cursor.moveToNext()) {
+                name = cursor.getString(nameIndex)
+                number = cursor.getString(numberIndex)
+                number = number.replace(" ", "")
+                if (!mobileNoSet.contains(number)) {
+                    contactList.add(ContactModel("", name, number))
+                    mobileNoSet.add(number)
+//                        Log.d(
+//                            "phone", "Phone Number: name = " + name
+//                                + " No = " + number
+//                        )
+                }
+            }
+
+        } catch (e: SecurityException) {
+            return contactList
+        } finally {
+            cursor.close()
+        }
+    }
+    return contactList
+}
+
+
+
